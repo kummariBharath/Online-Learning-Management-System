2ef4c1f8a7b1345166e0e1e1d440112529b9b56c
kummariBharath <bharathkummari2611@gmail.com>
2026-03-10 22:51:23 +0530

    feat: Create initial Django project settings with allauth, cloudinary, and deployment configurations.

--- Online-Learning-Management-System/OnlineLearningManagementSystem/skillmate/settings.py
+++ Online-Learning-Management-System/OnlineLearningManagementSystem/skillmate/settings.py
@@ -201,7 +201,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 DEFAULT_FROM_EMAIL = 'sudhatestmail@gmail.com'
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'bharathkummari010@gmail.com'
 EMAIL_PORT = 587
