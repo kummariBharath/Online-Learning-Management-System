cb7254f059801b3d515286f09ecf46e0edf2a0cd
kummariBharath <bharathkummari2611@gmail.com>
2026-03-10 21:13:58 +0530

    feat: initialize Django project settings with allauth, cloudinary, and whitenoise configurations.

--- Online-Learning-Management-System/OnlineLearningManagementSystem/skillmate/settings.py
+++ Online-Learning-Management-System/OnlineLearningManagementSystem/skillmate/settings.py
@@ -206,7 +206,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'bharathkummari010@gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_PASSWORD = 'nhbjssyxvxbendzz' 
+EMAIL_HOST_PASSWORD = 'pjcw jfwb rdhr zzmm' 
 
 # Cloudinary Django Integration
 
