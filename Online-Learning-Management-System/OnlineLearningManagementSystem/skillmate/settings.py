c9cc15a82f4fcb9ec490fc2d776c868ee20de956
kummariBharath <bharathkummari2611@gmail.com>
2026-03-10 19:51:22 +0530

    ..

--- Online-Learning-Management-System/OnlineLearningManagementSystem/skillmate/settings.py
+++ Online-Learning-Management-System/OnlineLearningManagementSystem/skillmate/settings.py
@@ -203,7 +203,7 @@ DEFAULT_FROM_EMAIL = 'sudhatestmail@gmail.com'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'sudhatestmail@gmail.com'
+EMAIL_HOST_USER = 'bharathkummari010@gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_PASSWORD = 'nhbjssyxvxbendzz' 
